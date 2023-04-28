cf03c8eb1d859d95ebf2dac9ce282e23e9db06c4
Sicelukwanda <sicelukwanda.zwane@gmail.com>
2023-04-28 17:29:35 +0100

    Add mesh loaders file Add .gitignore

---
+++ mesh_loaders.py
@@ -0,0 +1,45 @@
+
+class Mesh:
+
+    def __init__(
+        self, path = "./models/Ring/Ring.Ring.obj", 
+        scale = [0.5, 0.1, 0.1], 
+        shift = [0, 0, 0],
+        positionXYZ = [0, 0, 1], 
+        orientationXYZ = [0, 0, 0],
+        alpha = 1.0
+        ):
+        self.path = path
+        self.scale = scale
+        self.position = position
+        self.orientation = orientation
+        self.shift = shift
+
+        self.visualShapeId = p.createVisualShape(
+            shapeType=p.GEOM_MESH, 
+            fileName=self.path,
+            rgbaColor=[1.0, 1.0, 1.0, alpha], 
+            specularColor=[0.4, .4, 0],
+            meshScale=self.scale, 
+            visualFramePosition=self.shift
+            )
+        
+        self.collisionShapeId = p.createCollisionShape(
+            shapeType=p.GEOM_MESH,
+            fileName=self.path, 
+            meshScale=self.scale
+            )
+        
+        self.bodyId = p.createMultiBody(
+            baseMass=1.0,
+            baseCollisionShapeIndex=self.collisionShapeId,
+            baseVisualShapeIndex=self.visualShapeId,
+            basePosition=self.position,
+            baseOrientation=p.getQuaternionFromEuler(self.orientation)
+            )
+    
+    def getVisualShapeId(self):
+        return self.visualShapeId
+
+    def getCollisionShapeId(self):
+        return self.collisionShapeId
